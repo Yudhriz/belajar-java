424399f3cc3017e5b341466ceb954d89ea703293
Yudhriz <riskyyudha678@gmail.com>
2023-10-26 14:12:30 +0700

    Praktikum 5

---
+++ main.java
@@ -0,0 +1,14 @@
+class A {
+    void msg(){System.out.println("Hello");}
+}
+
+class B {
+    void msg(){System.out.println("Welcome");}
+}
+
+class C extends A,B {
+    public Static void main(String args[]){
+        C obj=new C();
+        obj.msg();
+}
+}
