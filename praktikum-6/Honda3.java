b6bff9326f74926b8fe627d99002a40fdbaa37db
Yudhriz <riskyyudha678@gmail.com>
2023-11-02 14:38:23 +0700

    Praktikum 06

---
+++ praktikum-6/Honda3.java
@@ -0,0 +1,12 @@
+class Bike {
+    int speedlimit = 150;
+}
+
+class Honda3 extends Bike {
+    int speedlimit = 90;
+
+    public static void main(String[] args) {
+        Bike obj = new Honda3();
+        System.out.println(obj.speedlimit);
+    }
+}
