25183f9a41390ddadc1c71d306b18816b5026a74
Yudhriz <riskyyudha678@gmail.com>
2023-11-30 13:14:13 +0700

    Praktikum 8

---
+++ praktikum-8/DeepCopyExample.java
@@ -0,0 +1,22 @@
+class ABC {
+    // instance variable of the class ABC
+    int x = 9;
+}
+
+public class DeepCopyExample {
+    // main method
+    public static void main(String argvs[]) {
+        // creating an object of the class ABC
+        ABC obj1 = new ABC();
+
+        // it will copy the reference, not value
+        ABC obj2 = new ABC();
+
+        // updating the value to 6
+        // using the reference variable obj2
+        obj2.x = 45;
+
+        // printing the value of x using reference variable obj1
+        System.out.println("The value of x is: " + obj1.x);
+    }
+}
