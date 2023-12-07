fe61073fb5be66a4d8878a20b7b5cbfda5d3acda
Yudhriz <riskyyudha678@gmail.com>
2023-12-07 13:18:30 +0700

    Praktikum 10

---
+++ praktikum-10/Samsung.java
@@ -0,0 +1,10 @@
+public class Samsung extends Company {  
+    @Override  
+        public int price(){   
+                        return 15;  
+    }  
+    @Override  
+    public String pack(){  
+             return "Samsung CD";  
+        }         
+}//End of the Samsung class.  
